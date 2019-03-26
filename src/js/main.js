0b180ae3d7345e47b4010ce6509e04d00213180f
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-03-26 12:33:43 +0100

    working build v3

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ const portfolioProject = [...document.querySelectorAll('.portfolio__project')]
 
 portfolioProject.forEach(project => (
    project.addEventListener('click', (e) => {
-      if (window.innerWidth < 500) {
+      if (window.innerWidth < 850 && e.target.nodeName != 'A') {
          const bcgChildren = [...e.target.children]
          bcgChildren.forEach(child => child.classList.toggle('visibilityToggler'))
          e.target.classList.toggle('backgroundToggler')
