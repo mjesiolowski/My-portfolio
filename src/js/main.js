da407b8432abedb98fa3f3821a68ee45de080ef2
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-07-04 09:30:32 +0200

    add footer and nav-arrow

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,24 @@ const Portfolio = {
    init() {
       this.desktopBreakpoint = 1000
       this.projects = document.querySelectorAll('.projects__container')
-
-      this.toggleVisibility = (e) => {
-         if (window.innerWidth < this.desktopBreakpoint && e.target.nodeName === 'DIV') {
-            const children = [...e.target.children]
-            children.forEach(child => child.classList.toggle('not-visible'))
-            e.target.classList.toggle('backgroundToggler')
-         }
-      }
+      this.footerDate = document.querySelector('.footer__date')
 
       this.projects.forEach(project => project.addEventListener('click', this.toggleVisibility))
+
+      this.setFooterDate()
+   },
+
+   toggleVisibility: (e) => {
+      if (window.innerWidth < this.desktopBreakpoint && e.target.nodeName === 'DIV') {
+         const children = [...e.target.children]
+         children.forEach(child => child.classList.toggle('not-visible'))
+         e.target.classList.toggle('backgroundToggler')
+      }
+   },
+
+   setFooterDate() {
+      this.footerDate.textContent = new Date().getFullYear()
    }
 }
-
 
 Portfolio.init()
