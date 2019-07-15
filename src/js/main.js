e8481fd700c0b0f256875e08b6e10c749f873a25
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-07-15 09:13:46 +0200

    this binding fix

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,12 @@ const Portfolio = {
       this.projects = document.querySelectorAll('.projects__container')
       this.footerDate = document.querySelector('.footer__date')
 
-      this.projects.forEach(project => project.addEventListener('click', this.toggleVisibility))
+      this.projects.forEach(project => project.addEventListener('click', (e) => this.toggleVisibility(e, this)))
 
       this.setFooterDate()
    },
 
-   toggleVisibility: (e) => {
+   toggleVisibility(e) {
       if (window.innerWidth < this.desktopBreakpoint && e.target.nodeName === 'DIV') {
          const children = [...e.target.children]
          children.forEach(child => child.classList.toggle('not-visible'))
